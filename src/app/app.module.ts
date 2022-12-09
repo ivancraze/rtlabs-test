307ff57281934e1285446b0b52847076f944014a
i.plotnikov <ploBcvxw3>
2022-12-09 19:52:32 +0400

    fix: Добавил сервис для логина

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent, SearchComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
