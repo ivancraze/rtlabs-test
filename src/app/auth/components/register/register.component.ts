9261987f8400bfd52925e7bb3cbf16536c238635
i.plotnikov <ploBcvxw3>
2022-12-10 17:08:30 +0400

    fix: убрал лишние файлы для регистрации и сервис логина

--- src/app/auth/components/register/register.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'rt-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
-})
-export class RegisterComponent {}
