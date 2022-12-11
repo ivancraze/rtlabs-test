6dadb3403fe849210d29ed2d2cb145e71d6c6534
i.plotnikov <ploBcvxw3>
2022-12-11 16:07:13 +0400

    feat: Добавил кнопку logout для выхода из аккаунта

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
+  constructor(private authService: AuthService) {}
 
+  onLogoutClick() {
+    this.authService.logout();
+  }
 }
