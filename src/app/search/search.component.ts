644be974d358471af5b96a7ee632825edf1c5f55
i.plotnikov <ploBcvxw3>
2022-12-12 16:30:23 +0400

    feat: Добавил поиск

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { debounceTime, distinctUntilChanged, map, Observable, OperatorFunction } from 'rxjs';
+
+const peoples: string[] = ['Иван', 'Алексей', 'Айрат', 'Сергей', 'Андрей', 'Айрат2', 'Андрей2'];
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   constructor(private authService: AuthService) {}
+
+  public autocompleteResult: string[] = [];
+  public searchResult: string[] = [];
+  public searchResultVisible: boolean = false;
+
+  autocomplete: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      map((term) =>
+        term.length < 2 ? [] : peoples.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)
+      )
+    );
+
+  onSearchButtonClick() {
+    this.searchResultVisible = true;
+    if (this.autocompleteResult && this.autocompleteResult.length) {
+      this.searchResult = this.autocompleteResult;
+    }
+  }
 
   onLogoutClick() {
     this.authService.logout();
+  }
+
+  ngOnInit(): void {
+    this.searchResultVisible = false;
   }
 }
